fe420794b7e208f0e0f84435977350f38c6c961c
Parvez Shaikh <parvezshaikhj@gmail.com>
2023-12-21 20:33:37 +0000

    Auth0 authentication library integrated

---
+++ auth0-integration-app/src/components/main-nav.js
@@ -0,0 +1,47 @@
+import {NavLink} from "react-router-dom";
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const MainNav = () => {
+  const  { loginWithRedirect, logout, user, isLoading } = useAuth0()
+  return (
+    <div className="navbar-nav mr-auto">
+      <NavLink
+        to="/"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/profile"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        Profile
+      </NavLink>
+      <NavLink
+        to="/external-api"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        External API
+      </NavLink>
+      {!isLoading && !user && (
+        <button className='btn btn-primary btn-block' onClick={() => loginWithRedirect()}>
+          Log In
+        </button>
+      )}
+      {!isLoading && user && (
+        <button className='btn btn-primary btn-block' onClick={() => logout()}>
+          Logout
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default MainNav;
